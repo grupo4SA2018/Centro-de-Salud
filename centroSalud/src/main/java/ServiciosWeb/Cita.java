883dca86496045af1ce1a716e140b7e3bd34984a
cyno <cyno@cynosure>
2018-03-02 20:54:38 -0600

    Se agrego el servicio de una cita

---
+++ centroSalud/src/main/java/ServiciosWeb/Cita.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServiciosWeb;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author cyno
+ */
+@WebService(serviceName = "Cita")
+public class Cita {
+
+    /**
+     * Registro de una cita
+     */
+     @WebMethod(operationName = "registro_Cita")
+   public String registro_Cita(@WebParam(name = "fecha") String fecha, @WebParam(name = "realizada") int realizada, @WebParam(name = "idPaciente") int paciente, @WebParam(name = "idDoctor") int doctor) throws SQLException {
+        String sql="";
+        Connection conn = null;
+        Statement stmt = null;
+        boolean result = false;
+        
+        try {
+           InitialContext ctx = new InitialContext();
+           DataSource ds = (DataSource)ctx.lookup("java:/CentroSaludDS");
+           conn =  ds.getConnection();
+            stmt = conn.createStatement();
+            
+            sql = "insert into Cita ( fecha,realizada,paciente,doctor)"+
+                    " values ('"+fecha+"',"+realizada+","+paciente+","+doctor+")";
+            
+            result = stmt.execute(sql);
+            result=true;
+            System.out.println(sql);
+            
+        } catch (NumberFormatException | SQLException | NamingException se) {
+            //Handle errors for JDBC
+            result = false;
+        }
+        finally {
+            //finally block used to close resources
+            if (stmt != null) {
+                conn.close();
+            } // do nothing
+            if (conn != null) {
+                conn.close();
+            } //end finally try
+        };
+        
+        
+        if(result)
+            return "{\"estado:exito\"}";
+        return "{\"estado:error\"}";
+    }
+}
