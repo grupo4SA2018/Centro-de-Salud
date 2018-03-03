9ead64fefe469c255506ce7b7685952b8859dcbb
cyno <cyno@cynosure>
2018-03-02 18:49:24 -0600

    Registro de medicamento

--- centroSalud/src/main/java/ServiciosWeb/RegistroMedicamento.java
+++ centroSalud/src/main/java/ServiciosWeb/RegistroMedicamento.java
@@ -5,9 +5,16 @@
  */
 package ServiciosWeb;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  *
@@ -17,10 +24,46 @@ import javax.jws.WebParam;
 public class RegistroMedicamento {
 
     /**
-     * This is a sample web service operation
+     * Registro de medicamento
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    
+    @WebMethod(operationName = "registro_Medicamento")
+    public String registro_Medicamento(@WebParam(name = "nombre") String nombre) throws SQLException {
+        String sql="";
+        Connection conn = null;
+        Statement stmt = null;
+        boolean result = false;
+        
+        try {
+           InitialContext ctx = new InitialContext();
+           DataSource ds = (DataSource)ctx.lookup("java:/CentroSaludDS");
+           conn =  ds.getConnection();
+            stmt = conn.createStatement();
+            
+            sql = "insert into Medicamento ( nombre)"+
+                    " values ('"+nombre+"')";
+            
+            result = stmt.execute(sql);
+            result=true;
+            System.out.println(sql);
+            
+        } catch (NumberFormatException | SQLException | NamingException se) {
+            //Handle errors for JDBC
+            result = false;
+        }
+        finally {
+            //finally block used to close resources
+            if (stmt != null) {
+                conn.close();
+            } // do nothing
+            if (conn != null) {
+                conn.close();
+            } //end finally try
+        };
+        
+        
+        if(result)
+            return "{\"estado:exito\"}";
+        return "{\"estado:error\"}";
     }
 }
