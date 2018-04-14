693be4de633ae484fca464cf49d695c702d3ca54
grupo4SA2018 <36547174+grupo4SA2018@users.noreply.github.com>
2018-04-14 11:13:42 -0600

    Agregar Modificacion CIta

--- centroSalud/src/main/java/ServiciosWeb/Cita.java
+++ centroSalud/src/main/java/ServiciosWeb/Cita.java
@@ -64,4 +64,42 @@ public class Cita {
             return "{\"estado:exito\"}";
         return "{\"estado:error\"}";
     }
+    @WebMethod(operationName = "update_Realizada")
+   public String update_Realizada(@WebParam(name = "idCita") String idCita) throws SQLException {
+        String sql="";
+        Connection conn = null;
+        Statement stmt = null;
+        boolean result = false;
+        
+        try {
+           InitialContext ctx = new InitialContext();
+           DataSource ds = (DataSource)ctx.lookup("java:/CentroSaludDS");
+           conn =  ds.getConnection();
+            stmt = conn.createStatement();
+            
+            sql = "update Cita set Realizada = 1 where idCita ="+idCita;
+            
+            stmt.execute(sql);
+            result=true;
+            System.out.println(sql);
+            
+        } catch (NumberFormatException | SQLException | NamingException se) {
+            //Handle errors for JDBC
+            result = false;
+        }
+        finally {
+            //finally block used to close resources
+            if (stmt != null) {
+                conn.close();
+            } // do nothing
+            if (conn != null) {
+                conn.close();
+            } //end finally try
+        };
+        
+        
+        if(result)
+            return "{\"estado:exito\"}";
+        return "{\"estado:error\"}";
+    }
 }
